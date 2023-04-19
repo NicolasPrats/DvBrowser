178c08ed2fa24b0fe722445ffc3d306f695209d2
Nicolas PRATS <nicolas.prats@avanade.com>
2023-04-19 18:09:25 +0200

    Add some input checks

--- Dataverse.Browser/UI/EnvironmentEditor.cs
+++ Dataverse.Browser/UI/EnvironmentEditor.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -37,11 +38,49 @@ namespace Dataverse.Browser.UI
 
         private void BtnOk_Click(object sender, EventArgs e)
         {
+            string hostName = GetValidHostName();
+            if (hostName == null)
+                return;
+            string assemblyPath = GetValidAssemblyPath();
             this.SelectedEnvironment = CurrentEnvironment ?? new EnvironnementConfiguration();
             this.SelectedEnvironment.Name = txtName.Text;
-            this.SelectedEnvironment.PluginAssemblies = new string[] { txtAssemblyPath.Text };
-            this.SelectedEnvironment.DataverseHost = txtHostName.Text;
+
+
+            this.SelectedEnvironment.PluginAssemblies = new string[] { assemblyPath };
+
+            this.SelectedEnvironment.DataverseHost = hostName;
             this.Close();
+        }
+
+        private string GetValidAssemblyPath()
+        {
+            string assemblyPath = txtAssemblyPath.Text;
+            if (!File.Exists(assemblyPath))
+            {
+                MessageBox.Show("File not found:" + assemblyPath);
+                return null;
+            }
+            return assemblyPath;
+        }
+
+        private string GetValidHostName()
+        {
+            string hostName = txtHostName.Text;
+            if (hostName.ToLowerInvariant().StartsWith("https://"))
+            {
+                hostName = hostName.Substring("https://".Length);
+            }
+            int indexOfSlash = hostName.IndexOf('/');
+            if (indexOfSlash > 0)
+            {
+                hostName = hostName.Substring(0, indexOfSlash);
+            }
+            if (string.IsNullOrEmpty(hostName))
+            {
+                MessageBox.Show("Invalid hostname!");
+                return null;
+            }
+            return hostName;
         }
     }
 }
