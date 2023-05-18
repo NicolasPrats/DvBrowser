b2e570ce922bd2207fedc64bc8b66741d4054ef2
Nicolas PRATS <nicolas.prats@gmail.com>
2023-05-18 23:08:16 +0200

    Minor fixes

--- Dataverse.Browser/UI/EnvironmentEditor.Designer.cs
+++ Dataverse.Browser/UI/EnvironmentEditor.Designer.cs
@@ -46,101 +46,103 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(9, 59);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(12, 73);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(294, 13);
+            this.label1.Size = new System.Drawing.Size(369, 16);
             this.label1.TabIndex = 1;
             this.label1.Text = "Environment hostname (eg: myinstance.crm4.dynamics.com):";
             // 
             // txtHostName
             // 
-            this.txtHostName.Location = new System.Drawing.Point(11, 75);
-            this.txtHostName.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtHostName.Location = new System.Drawing.Point(15, 92);
+            this.txtHostName.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtHostName.Name = "txtHostName";
-            this.txtHostName.Size = new System.Drawing.Size(320, 20);
+            this.txtHostName.Size = new System.Drawing.Size(425, 22);
             this.txtHostName.TabIndex = 2;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(9, 106);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(12, 130);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(255, 13);
+            this.label2.Size = new System.Drawing.Size(327, 16);
             this.label2.TabIndex = 3;
             this.label2.Text = "Assembly Path (preferably compiled in debug mode): ";
             // 
             // txtAssemblyPath
             // 
-            this.txtAssemblyPath.Location = new System.Drawing.Point(9, 122);
-            this.txtAssemblyPath.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtAssemblyPath.Location = new System.Drawing.Point(12, 150);
+            this.txtAssemblyPath.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtAssemblyPath.Name = "txtAssemblyPath";
-            this.txtAssemblyPath.Size = new System.Drawing.Size(296, 20);
+            this.txtAssemblyPath.Size = new System.Drawing.Size(393, 22);
             this.txtAssemblyPath.TabIndex = 3;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(9, 19);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Location = new System.Drawing.Point(12, 23);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(179, 13);
+            this.label3.Size = new System.Drawing.Size(222, 16);
             this.label3.TabIndex = 5;
             this.label3.Text = "Choose a name for this environment:";
             // 
             // txtName
             // 
-            this.txtName.Location = new System.Drawing.Point(11, 34);
-            this.txtName.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtName.Location = new System.Drawing.Point(15, 42);
+            this.txtName.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtName.Name = "txtName";
-            this.txtName.Size = new System.Drawing.Size(320, 20);
+            this.txtName.Size = new System.Drawing.Size(425, 22);
             this.txtName.TabIndex = 1;
             // 
             // btnOk
             // 
-            this.btnOk.Location = new System.Drawing.Point(274, 171);
-            this.btnOk.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnOk.Location = new System.Drawing.Point(365, 210);
+            this.btnOk.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnOk.Name = "btnOk";
-            this.btnOk.Size = new System.Drawing.Size(56, 19);
+            this.btnOk.Size = new System.Drawing.Size(75, 23);
             this.btnOk.TabIndex = 4;
             this.btnOk.Text = "Go";
             this.btnOk.UseVisualStyleBackColor = true;
             this.btnOk.Click += new System.EventHandler(this.BtnOk_Click);
             // 
-
             // cbDisableAsyncSteps
             // 
             this.cbDisableAsyncSteps.AutoSize = true;
-            this.cbDisableAsyncSteps.Location = new System.Drawing.Point(15, 179);
+            this.cbDisableAsyncSteps.Location = new System.Drawing.Point(15, 187);
+            this.cbDisableAsyncSteps.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.cbDisableAsyncSteps.Name = "cbDisableAsyncSteps";
             this.cbDisableAsyncSteps.Size = new System.Drawing.Size(199, 20);
             this.cbDisableAsyncSteps.TabIndex = 6;
             this.cbDisableAsyncSteps.Text = "Disable asynchronous steps";
             this.toolTip1.SetToolTip(this.cbDisableAsyncSteps, resources.GetString("cbDisableAsyncSteps.ToolTip"));
             this.cbDisableAsyncSteps.UseVisualStyleBackColor = true;
-
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.AutoPopDelay = 20000;
+            this.toolTip1.InitialDelay = 500;
+            this.toolTip1.ReshowDelay = 100;
+            // 
             // openFileDialog
             // 
             this.openFileDialog.FileName = "openFileDialog1";
             // 
             // btnSelectAssembly
             // 
-            this.btnSelectAssembly.Location = new System.Drawing.Point(307, 122);
+            this.btnSelectAssembly.Location = new System.Drawing.Point(409, 150);
+            this.btnSelectAssembly.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnSelectAssembly.Name = "btnSelectAssembly";
-            this.btnSelectAssembly.Size = new System.Drawing.Size(24, 20);
+            this.btnSelectAssembly.Size = new System.Drawing.Size(32, 25);
             this.btnSelectAssembly.TabIndex = 6;
             this.btnSelectAssembly.Text = "...";
             this.btnSelectAssembly.UseVisualStyleBackColor = true;
             this.btnSelectAssembly.Click += new System.EventHandler(this.btnSelectAssembly_Click);
-
             // 
             // EnvironmentEditor
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-
-            this.ClientSize = new System.Drawing.Size(449, 246);
+            this.ClientSize = new System.Drawing.Size(453, 243);
             this.Controls.Add(this.cbDisableAsyncSteps);
             this.Controls.Add(this.btnSelectAssembly);
             this.Controls.Add(this.btnOk);
@@ -152,7 +154,7 @@
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.Name = "EnvironmentEditor";
             this.Text = "Environment Settings";
