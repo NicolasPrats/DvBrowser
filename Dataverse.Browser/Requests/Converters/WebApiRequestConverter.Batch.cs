2880dd77f83e37ac1799c35f36705bb36cd689b1
NicolasPrats <33152924+NicolasPrats@users.noreply.github.com>
2023-05-25 08:55:15 +0200

    Features/customapi (#33)
    Unbound custom api
    Entity bound custom api
    Input parameters with a primitive type
    Input parameters with a complex type
    Output parameters with a primitive type

---
+++ Dataverse.Browser/Requests/Converters/WebApiRequestConverter.Batch.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Activities.DurableInstancing;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+using System.Text.RegularExpressions;
+using System.Windows.Shapes;
+using CefSharp;
+using CefSharp.DevTools.DOM;
+using Dataverse.Browser.Constants;
+using Dataverse.Browser.Context;
+using Dataverse.Browser.Requests.SimpleClasses;
+using Microsoft.OData.Edm;
+using Microsoft.OData.UriParser;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Messages;
+using Microsoft.Xrm.Sdk.Metadata;
+using Microsoft.Xrm.Sdk.Query;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Tab;
+
+namespace Dataverse.Browser.Requests.Converter
+{
+    internal partial class WebApiRequestConverter
+
+    {
+       
+
+        private OrganizationRequest ConvertToExecuteMultipleRequest(InterceptedWebApiRequest webApiRequest)
+        {
+            var originRequest = webApiRequest.SimpleHttpRequest.OriginRequest;
+            string contentType = originRequest.Headers["Content-Type"];
+            if (!contentType.StartsWith("multipart/mixed;"))
+            {
+                throw new NotImplementedException("ContentType " + contentType + " is not supported for batch requests");
+            }
+
+            ExecuteMultipleRequest executeMultipleRequest = new ExecuteMultipleRequest()
+            {
+                Requests = new OrganizationRequestCollection()
+            };
+
+            MemoryStream dataStream = AddMissingLF(originRequest);
+            using (var content = new StreamContent(dataStream))
+            {
+                //TODO support des changesets
+                //TODO support des continue on error
+                content.Headers.ContentType = MediaTypeHeaderValue.Parse(contentType);
+
+                MultipartMemoryStreamProvider provider = content.ReadAsMultipartAsync().Result;
+
+                //TODO changesets
+                foreach (var httpContent in provider.Contents)
+                {
+                    var data = httpContent.ReadAsByteArrayAsync().Result;
+                    var innerRequest = CreateSimplifiedRequestFromMimeMessage(data);
+                    var convertedRequest = this.ConvertUnknowSimplifiedRequestToOrganizationRequest(innerRequest);
+                    if (convertedRequest == null)
+                    {
+                        throw new NotSupportedException("Only web api requests are supported!");
+                    }
+                    else
+                    if (convertedRequest.ConvertedRequest != null)
+                    {
+                        executeMultipleRequest.Requests.Add(convertedRequest.ConvertedRequest);
+                    }
+                    else
+                    {
+                        throw new NotSupportedException("One inner request could not be converted:" + convertedRequest.ConvertFailureMessage);
+                    }
+                }
+
+            }
+            return executeMultipleRequest;
+        }
+
+        private SimpleHttpRequest CreateSimplifiedRequestFromMimeMessage(byte[] data)
+        {
+            var request = new SimpleHttpRequest();
+            int index = Array.FindIndex(data, b => b == (byte)'\r');
+            if (index == -1)
+            {
+                throw new NotSupportedException("Unable to parse data, no \\r found!");
+            }
+            string firstLine = Encoding.UTF8.GetString(data, 0, index);
+            if (!firstLine.StartsWith("GET"))
+            {
+                throw new ApplicationException("Unable to parse first line: " + firstLine);
+
+            }
+            string url = firstLine.Substring(4);
+            if (url.EndsWith("HTTP/1.1"))
+            {
+                url = url.Substring(0, url.Length - 8);
+            }
+            request.Method = "GET";
+            request.LocalPathWithQuery = url;
+            //TODO : body and headers
+            return request;
+        }
+
+        private static MemoryStream AddMissingLF(IRequest request)
+        {
+            // Les requêtes batch de CRM contiennent uniquement des LF en séparateurs de lignes et pas de CR
+            var data = request.PostData.Elements.FirstOrDefault().Bytes;
+            MemoryStream dataStream = new MemoryStream();
+            bool previousIsCr = false;
+            for (int i = 0; i < data.Length; i++)
+            {
+                var value = data[i];
+                if (value == '\r')
+                {
+                    previousIsCr = true;
+                }
+                else
+                {
+                    if (value == '\n' && !previousIsCr)
+                    {
+                        dataStream.WriteByte((byte)'\r');
+                    }
+                    previousIsCr = false;
+                }
+                dataStream.WriteByte(value);
+            }
+            dataStream.Seek(0, SeekOrigin.Begin);
+            return dataStream;
+        }
+
+    }
+}
---
+++ Dataverse.Browser/Requests/Converters/WebApiRequestConverter.CustomApi.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Activities;
+using System.Activities.DurableInstancing;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.Json;
+using System.Text.RegularExpressions;
+using System.Windows.Shapes;
+using System.Xml;
+using CefSharp;
+using CefSharp.DevTools.DOM;
+using Dataverse.Browser.Constants;
+using Dataverse.Browser.Context;
+using Dataverse.Browser.Requests.SimpleClasses;
+using Microsoft.Crm.Sdk.Messages;
+using Microsoft.OData.Edm;
+using Microsoft.OData.UriParser;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Messages;
+using Microsoft.Xrm.Sdk.Metadata;
+using Microsoft.Xrm.Sdk.Query;
+using static System.Net.WebRequestMethods;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Tab;
+
+namespace Dataverse.Browser.Requests.Converter
+{
+    internal partial class WebApiRequestConverter
+    {
+        private void ConvertToAction(IEdmOperation operation, InterceptedWebApiRequest webApiRequest, bool isBound)
+        {
+            OrganizationRequest request = new OrganizationRequest(operation.Name);
+            string boundParameterName = null;
+            if (isBound)
+            {
+                boundParameterName = operation.Parameters.First().Name;
+            }
+            using (JsonDocument json = JsonDocument.Parse(webApiRequest.SimpleHttpRequest.Body))
+            {
+                foreach (var node in json.RootElement.EnumerateObject())
+                {
+                    string key = node.Name;
+                    var parameter = operation.FindParameter(key);
+                    if (parameter == null)
+                    {
+                        throw new NotSupportedException($"parameter {key} not found!");
+                    }
+                    if (key == boundParameterName)
+                    {
+                        key = "Target";
+                    }
+                    request[key] = ConvertValueToAttribute(node.Value, parameter.Type);
+                }
+            }
+            webApiRequest.ConvertedRequest = request;
+        }
+
+        private object ConvertValueToAttribute(JsonElement value, IEdmTypeReference type)
+        {
+            switch (type.FullName())
+            {
+                case "Edm.Boolean":
+                    return value.GetBoolean();
+                case "Edm.Byte":
+                    return value.GetByte();
+                case "Edm.DateTime":
+                    return value.GetDateTime();
+                case "Edm.Decimal":
+                    return value.GetDecimal();
+                case "Edm.Double":
+                    return value.GetDouble();
+                case "Edm.Single":
+                    return value.GetSingle();
+                case "Edm.Guid":
+                    return value.GetGuid();
+                case "Edm.Int16":
+                    return value.GetInt16();
+                case "Edm.Int32":
+                    return value.GetInt32();
+                case "Edm.Int64":
+                    return value.GetInt64();
+                case "Edm.SByte":
+                    return value.GetSByte();
+                case "Edm.String":
+                    return value.GetString();
+                default:
+                    if (type.TypeKind() == EdmTypeKind.Entity)
+                    {
+                        string typeName = type.FullName();
+                        return ConvertToEntityReference(value, type, typeName);
+                    }
+                    else if (type.TypeKind() == EdmTypeKind.Collection)
+                    {
+                        EntityReferenceCollection collection = new EntityReferenceCollection();
+                        foreach (var item in value.EnumerateArray())
+                        {
+                            collection.Add(ConvertToEntityReference(item, null, null));
+                        }
+                        return collection;
+                    }
+                    else
+                    {
+                        throw new NotImplementedException($"Type {type.TypeKind()} is not implemented!");
+                    }
+
+            }
+            throw new NotSupportedException("Type is unknown:" + type.FullName());
+        }
+
+        private  EntityReference ConvertToEntityReference(JsonElement value, IEdmTypeReference type, string typeName)
+        {
+            if (!value.TryGetProperty("@odata.type", out var dataType))
+            {
+                throw new NotSupportedException("@odata.type property must be set!");
+            }
+            if (typeName != null && dataType.GetString() != typeName)
+            {
+                throw new NotSupportedException($"@odata.type property is of type {dataType.GetString()} whereas {typeName} was expected!");
+            }
+            typeName = dataType.GetString();
+            IEdmEntityType definition;
+            if (type == null)
+            {
+                definition = this.Context.Model.FindType(typeName) as IEdmEntityType;
+            }
+            else
+            {
+                definition = type?.Definition as IEdmEntityType;
+            }
+            if (definition == null)
+            {
+                throw new NotSupportedException($"IEdmEntityType was expected!");
+            }
+            var key = definition.DeclaredKey.FirstOrDefault()?.Name;
+            if (!value.TryGetProperty(key, out var id))
+            {
+                throw new NotSupportedException($"@{key} property must be set!");
+            }
+            return new EntityReference(definition.Name,new Guid( id.GetString()));
+        }
+    }
+}
