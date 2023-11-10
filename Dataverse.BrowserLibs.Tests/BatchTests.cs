882f5973bc0d68ad50f079bb7beeefb1c1f0a5b7
Nicolas PRATS <nicolas.prats@gmail.com>
2023-11-10 11:52:16 +0100

    Parse des batch sans changeset

---
+++ Dataverse.BrowserLibs.Tests/BatchTests.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using Dataverse.WebApi2IOrganizationService.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Dataverse.BrowserLibs.Tests
+{
+    [TestClass]
+    public class BatchTests
+    {
+
+        public TestContext TestContext { get; set; }
+
+        [TestMethod]
+        public void CreateOpportunity_ChangesetNoPlugin()
+        {
+            WebApiRequest webApiRequest = WebApiRequest.CreateFromLocalPathWithQuery(
+                "POST",
+                "/api/data/v9.2/$batch",
+                new System.Collections.Specialized.NameValueCollection()
+                {
+                    {"Content-Type", "multipart/mixed;boundary=batch_1699608839380" },
+                    {"Prefer" ,"return=representation"},
+                },
+                @"--batch_1699608839380
+Content-Type: multipart/mixed;boundary=changeset_1699608839381
+
+--changeset_1699608839381
+Content-Type: application/http
+Content-Transfer-Encoding: binary
+Content-ID: 1
+
+POST /api/data/v9.0/opportunities HTTP/1.1
+Accept: application/json
+MSCRM.SuppressDuplicateDetection: false
+Content-Type: application/json
+Prefer: odata.include-annotations=""*""
+ClientHost: Browser
+
+{""name"":""test"",""decisionmaker"":false,""processid"":""00000000-0000-0000-0000-000000000000"",""isrevenuesystemcalculated"":false,""msdyn_ordertype"":192350000,""msdyn_forecastcategory"":100000001,""transactioncurrencyid@odata.bind"":""/transactioncurrencies(64b134ec-6c49-ee11-be6e-000d3ab1cf95)"",""transactioncurrencyid@OData.Community.Display.V1.FormattedValue"":""US Dollar"",""statuscode"":1,""statecode"":0,""ownerid@odata.bind"":""/systemusers(4c6c3e24-0f49-ee11-be6e-000d3ab1cf95)"",""ownerid@OData.Community.Display.V1.FormattedValue"":""System Administrator"",""opportunityid"":""f3cc4ab4-4dc1-4585-93a3-ff8acfc4b6f1""}
+--changeset_1699608839381
+Content-Type: application/http
+Content-Transfer-Encoding: binary
+Content-ID: 2
+
+POST /api/data/v9.0/opportunitysalesprocesses HTTP/1.1
+Accept: application/json
+MSCRM.SuppressDuplicateDetection: false
+Content-Type: application/json
+Prefer: odata.include-annotations=""*""
+ClientHost: Browser
+
+{""processid@odata.bind"":""/workflows(3e8ebee6-a2bc-4451-9c5f-b146b085413a)"",""opportunityid@odata.bind"":""/opportunities(f3cc4ab4-4dc1-4585-93a3-ff8acfc4b6f1)"",""traversedpath"":""6b9ce798-221a-4260-90b2-2a95ed51a5bc"",""activestageid@odata.bind"":""/processstages(6b9ce798-221a-4260-90b2-2a95ed51a5bc)""}
+--changeset_1699608839381--
+
+--batch_1699608839380--
+"
+                );
+            Helper.TestAgainstExpected(this.TestContext, webApiRequest);
+        }
+
+        [TestMethod]
+        public void CreateOpportunity_NoChangesetNoPlugin()
+        {
+            Guid oppId = Guid.NewGuid();
+            WebApiRequest webApiRequest = WebApiRequest.CreateFromLocalPathWithQuery(
+                "POST",
+                "/api/data/v9.2/$batch",
+                new System.Collections.Specialized.NameValueCollection()
+                {
+                    {"Content-Type", "multipart/mixed;boundary=batch_1699608839380" },
+                    {"Prefer" ,"return=representation"},
+                },
+                @"--batch_1699608839380
+Content-Type: application/http
+Content-Transfer-Encoding: binary
+Content-ID: 1
+
+POST /api/data/v9.0/opportunities HTTP/1.1
+Accept: application/json
+MSCRM.SuppressDuplicateDetection: false
+Content-Type: application/json
+Prefer: odata.include-annotations=""*""
+ClientHost: Browser
+
+{""name"":""test"",""decisionmaker"":false,""processid"":""00000000-0000-0000-0000-000000000000"",""isrevenuesystemcalculated"":false,""msdyn_ordertype"":192350000,""msdyn_forecastcategory"":100000001,""transactioncurrencyid@odata.bind"":""/transactioncurrencies(64b134ec-6c49-ee11-be6e-000d3ab1cf95)"",""transactioncurrencyid@OData.Community.Display.V1.FormattedValue"":""US Dollar"",""statuscode"":1,""statecode"":0,""ownerid@odata.bind"":""/systemusers(4c6c3e24-0f49-ee11-be6e-000d3ab1cf95)"",""ownerid@OData.Community.Display.V1.FormattedValue"":""System Administrator"",""opportunityid"":""" + oppId + @"""}
+--batch_1699608839380
+Content-Type: application/http
+Content-Transfer-Encoding: binary
+Content-ID: 2
+
+POST /api/data/v9.0/opportunitysalesprocesses HTTP/1.1
+Accept: application/json
+MSCRM.SuppressDuplicateDetection: false
+Content-Type: application/json
+Prefer: odata.include-annotations=""*""
+ClientHost: Browser
+
+{""processid@odata.bind"":""/workflows(3e8ebee6-a2bc-4451-9c5f-b146b085413a)"",""opportunityid@odata.bind"":""/opportunities(" + oppId + @")"",""traversedpath"":""6b9ce798-221a-4260-90b2-2a95ed51a5bc"",""activestageid@odata.bind"":""/processstages(6b9ce798-221a-4260-90b2-2a95ed51a5bc)""}
+
+--batch_1699608839380--
+"
+                );
+            Helper.TestAgainstExpected(this.TestContext, webApiRequest);
+        }
+
+
+    }
+}
--- Dataverse.WebApi2IOrganizationService/Converters/RequestConverter.Batch.cs
+++ Dataverse.WebApi2IOrganizationService/Converters/RequestConverter.Batch.cs
@@ -12,7 +12,7 @@ namespace Dataverse.WebApi2IOrganizationService.Converters
 {
     public partial class RequestConverter
     {
-        private OrganizationRequest ConvertToExecuteMultipleRequest(RequestConversionResult conversionResult)
+        private void ConvertToExecuteMultipleRequest(RequestConversionResult conversionResult)
         {
             var originRequest = conversionResult.SrcRequest;
             string contentType = originRequest.Headers["Content-Type"];
@@ -35,17 +35,11 @@ namespace Dataverse.WebApi2IOrganizationService.Converters
 
                 MultipartMemoryStreamProvider provider = content.ReadAsMultipartAsync().Result;
 
-                //TODO changesets
                 foreach (var httpContent in provider.Contents)
                 {
                     var data = httpContent.ReadAsByteArrayAsync().Result;
                     var innerRequest = CreateSimplifiedRequestFromMimeMessage(data);
-                    var convertedRequest = Convert(innerRequest);
-                    if (convertedRequest == null)
-                    {
-                        throw new NotSupportedException("Only web api requests are supported!");
-                    }
-                    else
+                    var convertedRequest = Convert(innerRequest) ?? throw new NotSupportedException("Only web api requests are supported!");
                     if (convertedRequest.ConvertedRequest != null)
                     {
                         executeMultipleRequest.Requests.Add(convertedRequest.ConvertedRequest);
@@ -57,29 +51,40 @@ namespace Dataverse.WebApi2IOrganizationService.Converters
                 }
 
             }
-            return executeMultipleRequest;
+            executeMultipleRequest.Settings = new ExecuteMultipleSettings()
+            {
+                ContinueOnError = true,
+                ReturnResponses = true
+            };
+            conversionResult.ConvertedRequest = executeMultipleRequest;
         }
 
         private WebApiRequest CreateSimplifiedRequestFromMimeMessage(byte[] data)
         {
-            int index = Array.FindIndex(data, b => b == (byte)'\r');
-            if (index == -1)
-            {
-                throw new NotSupportedException("Unable to parse data, no \\r found!");
-            }
-            string firstLine = Encoding.UTF8.GetString(data, 0, index);
-            if (!firstLine.StartsWith("GET"))
-            {
-                throw new ApplicationException("Unable to parse first line: " + firstLine);
+            string requestString = Encoding.ASCII.GetString(data);
+            // Split the request string into lines
+            string[] requestLines = requestString.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
 
-            }
-            string url = firstLine.Substring(4);
-            if (url.EndsWith("HTTP/1.1"))
+            // First line contains the request method, URL, and HTTP version
+            string[] firstLineParts = requestLines[0].Split(' ');
+            string method = firstLineParts[0];
+            string url = firstLineParts[1];
+
+            // Parse headers starting from the second line
+            int bodyIndex = Array.IndexOf(requestLines, ""); // Find the index of the empty line that separates headers and body
+            NameValueCollection headers = new NameValueCollection();
+            for (int i = 1; i < bodyIndex; i++)
             {
-                url = url.Substring(0, url.Length - 8);
+                string[] headerParts = requestLines[i].Split(':');
+                string headerName = headerParts[0].Trim();
+                string headerValue = headerParts[1].Trim();
+                headers.Add(headerName, headerValue);
             }
-            var request = new WebApiRequest("GET", url, new NameValueCollection(), null);
-            //TODO : body and headers
+
+            // Extract and display the body.
+            // TODO: \r may have be stripped here
+            string body = string.Join("\n", requestLines, bodyIndex + 1, requestLines.Length - bodyIndex - 1);
+            var request = new WebApiRequest(method, url, headers, body);
             return request;
         }
 
