0dbfd8849bdf38e07231961242086dc546520a79
Nicolas PRATS <nicolas.prats@gmail.com>
2024-02-07 13:28:49 +0100

    Added more explicit message when a plugin type cannot be loaded. Assembly Microsoft.Xrm.Sdk is always loaded from the DataverseBrowser path

--- Dataverse.Plugin.Emulator/PluginInstanceCache.cs
+++ Dataverse.Plugin.Emulator/PluginInstanceCache.cs
@@ -1,11 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
 using Microsoft.Xrm.Sdk;
 
 namespace Dataverse.Plugin.Emulator.Steps
 {
     internal class PluginInstanceCache
     {
+        static PluginInstanceCache()
+        {
+            AppDomain.CurrentDomain.AssemblyResolve += CurrentDomain_AssemblyResolve;
+
+        }
+
+        private static System.Reflection.Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
+        {
+            if (args.Name.StartsWith("Microsoft.Xrm.Sdk"))
+            {
+                string path = Path.GetDirectoryName(typeof(PluginInstanceCache).Assembly.Location);
+                //We ignore the version so that we can load dll compiled with an older version of the Sdk
+                return Assembly.LoadFile(Path.Combine(path, "Microsoft.Xrm.Sdk.dll"));
+            }
+            return null;
+        }
+
         private readonly Dictionary<ValueTuple<string, string, string>, IPlugin> Cache = new Dictionary<ValueTuple<string, string, string>, IPlugin>();
         public IPlugin GetPlugin(PluginStepDescription step)
         {
@@ -13,7 +32,7 @@ namespace Dataverse.Plugin.Emulator.Steps
 
             if (!this.Cache.TryGetValue(key, out var plugin))
             {
-                var type = step.Assembly.GetType(step.EventHandler);
+                var type = step.Assembly.GetType(step.EventHandler, true);
                 if (type.GetConstructor(new Type[] { }) != null)
                 {
                     plugin = (IPlugin)Activator.CreateInstance(type);
